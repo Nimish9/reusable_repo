025524f9ff98e9de534626065e30f0a2e949a13f
Nimish <sorathiyanimish9@gmail.com>
2025-12-11 14:48:46 +0100

    changed a code

--- test/e2e/test_e2e_example.py
+++ test/e2e/test_e2e_example.py
@@ -1,5 +1,10 @@
 import requests
+from time import sleep
 
 def test_ping_service():
-    resp = requests.get("https://httpbin.org/get")
+    for _ in range(3):
+        resp = requests.get("https://httpbin.org/get")
+        if resp.status_code == 200:
+            break
+        sleep(1)  # wait a second and retry
     assert resp.status_code == 200
